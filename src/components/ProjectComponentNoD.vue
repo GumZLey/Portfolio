97c3bd0f790d89340ae4336a04c074b2b5ba0ff1
6513177-Ananda <123048565+6513177-Ananda@users.noreply.github.com>
2025-04-14 22:25:45 +0700

    Add more project

---
+++ src/components/ProjectComponentNoD.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="flex sm:flex-col-reverse justify-around items-center w-full min-h-[500px] p-10">
+    <div class="flex flex-row justify-center items-center text-left gap-2">
+      <h1 class="text-4xl font-bold mt-2 leading-15 ml-1">
+        {{ ProjectDataNoD.name }}
+      </h1>
+      <img
+        src="@/assets/github.png"
+        class="py-2 px-2 w-14 h-14 mt-2 hover:text-primary transition-colors duration-300 text-primary/80 underline cursor-pointer"
+        @click.prevent="toGithub"
+        alt="Github"
+      />
+    </div>
+    <div class="flex items-center justify-center">
+      <img :src="imagePath" alt="My Profile" class="w-[600px] h-auto rounded-2xl object-cover" />
+    </div>
+  </section>
+</template>
+
+<script setup>
+const props = defineProps({
+  ProjectDataNoD: Object,
+})
+
+const imagePath = new URL(`../assets/${props.ProjectDataNoD.image}`, import.meta.url).href
+
+const toGithub = () => {
+  window.open(props.ProjectDataNoD.link, '_blank')
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { HiChevronDoubleDown } from 'vue-icons-plus/hi'
 import { ref, onMounted } from 'vue'
 import ProjectComponent from '@/components/ProjectComponent.vue'
+import ProjectComponentNoD from '@/components/ProjectComponentNoD.vue'
 
 // Dynamically import all images from the skills folder
 const skillImages = ref([])
@@ -11,16 +12,114 @@ const projects = ref([
   {
     id: 1,
     name: 'Image Segment Nothing',
-    description: 'This project is a web-based Deep-Learning integration that provides advanced image segmentation capabilities. Users can upload images, process them using AI-powered segmentation, visualize segmented regions, and download the results.',
+    description:
+      'This project is a web-based Deep-Learning integration that provides advanced image segmentation capabilities. Users can upload images, process them using AI-powered segmentation, visualize segmented regions, and download the results.',
     image: 'SN.png',
     link: 'https://github.com/GumZLey/image-segment-nothing-project',
   },
   {
     id: 2,
     name: 'Contact List',
-    description: 'Contact List is a web app designed to make managing contacts simple and convenient. Users can securely log in with Google, then add, edit, delete, and search through their contact list. The frontend is built with Vue 3, Vuex for state management, and Tailwind CSS for styling. Axios handles API calls. The backend runs on Node.js with Express.js. MongoDB stores user and contact data. Security is handled with md5 for password hashing and dotenv for managing environment variables. Development tools like ESLint, Prettier keep the code clean and consistent.',
+    description:
+      'Contact List is a web app designed to make managing contacts simple and convenient. Users can securely log in with Google, then add, edit, delete, and search through their contact list. The frontend is built with Vue 3, Vuex for state management, and Tailwind CSS for styling. Axios handles API calls. The backend runs on Node.js with Express.js. MongoDB stores user and contact data. Security is handled with md5 for password hashing and dotenv for managing environment variables. Development tools like ESLint, Prettier keep the code clean and consistent.',
     image: 'Contact-List-Login.png',
     link: 'https://github.com/GumZLey/Contact-List',
+  },
+  {
+    id: 3,
+    name: 'Locus',
+    description:
+      'The Locus, project handles various game elements like players, projectiles, and objects, all while keeping the UI responsive. It’s built with Java and uses the Swing framework for graphical components like the game window, settings, credits, and a "how-to-play" guide. Sound effects and background music are added using Java’s javax.sound.sampled package. The code follows object-oriented principles, with classes like GameWindow, Player, and Projectile. The project is mainly focused on Thread.',
+    image: 'Locus.png',
+    link: 'https://github.com/GumZLey/Locus',
+  },
+  {
+    id: 4,
+    name: 'Agado (Ongoing)',
+    description:
+      "Agado is like an Agoda clone, but different. This website will collect data on the dormitory near Mahidol University and filter the best price value for the dormitory. I'll use Google Map API for the location, PostgreSQL for the database, Node Express for the backend, and Vue for the frontend. The seller can also list their place on the website.",
+    image: 'Agado.png',
+    link: 'https://github.com/GumZLey',
+  },
+  {
+    id: 5,
+    name: 'Dead Prevention (Ongoing)',
+    description:
+      "The Dead Prevention. In this project, We'll use an Embedded system to send the data shown in the card. We use Strapi as a backend data management and show data in Vue. This project will save patients who need help while waiting for a doctor. The products will attach to the finger of the patient to make the patient not feel uncomfortable.",
+    image: 'Dead_Prevention.png',
+    link: 'https://github.com/GumZLey',
+  },
+])
+
+const projectImagesFM = ref([
+  {
+    id: 1,
+    name: 'NFT preview card',
+    image: 'nft-preview.png',
+    link: 'https://github.com/GumZLey/nft-preview-card-component-main',
+  },
+  {
+    id: 2,
+    name: 'QR code component',
+    image: 'QR.png',
+    link: 'https://github.com/GumZLey/qr-code-component-Practice',
+  },
+  {
+    id: 3,
+    name: 'Result Summary',
+    image: 'Result.png',
+    link: 'https://github.com/GumZLey/FEM-Result_summary',
+  },
+])
+
+const projectImagesODIN = ref([
+  {
+    id: 1,
+    name: 'Calculator',
+    image: 'Calculator.png',
+    link: 'https://github.com/GumZLey/git_test/tree/main/Project/Calculator',
+  },
+  {
+    id: 2,
+    name: 'Dasshboard',
+    image: 'Dashboard.png',
+    link: 'https://github.com/GumZLey/git_test/tree/main/Project/Dashboard',
+  },
+  {
+    id: 3,
+    name: 'Etch A Sketch',
+    image: 'Etch.png',
+    link: 'https://github.com/GumZLey/git_test/tree/main/Project/Etch-a-Sketch',
+  },
+  {
+    id: 4,
+    name: 'Library',
+    image: 'Library.png',
+    link: 'https://github.com/GumZLey/git_test/tree/main/Project/Library',
+  },
+  {
+    id: 5,
+    name: 'Sign up Form',
+    image: 'Sign-up.png',
+    link: 'https://github.com/GumZLey/git_test/tree/main/Project/Sign-up-Form',
+  },
+  {
+    id: 6,
+    name: 'Tic Tac Toe',
+    image: 'Tic.png',
+    link: 'https://github.com/GumZLey/git_test/tree/main/Project/Tic-Tac-Toe',
+  },
+  {
+    id: 7,
+    name: 'Landing Page',
+    image: 'Landing.png',
+    link: 'https://github.com/GumZLey/git_test/tree/main/Project/landing-page',
+  },
+  {
+    id: 8,
+    name: 'Recipes',
+    image: 'Recipes.png',
+    link: 'https://github.com/GumZLey/git_test/tree/main/Project/recipes',
   },
 ])
 
@@ -90,9 +189,9 @@ onMounted(() => {
 </script>
 
 <template>
-  <header class=" transition-all duration-700 ease-in-out translate-y-[-50px] opacity-0">
+  <header class="transition-all duration-700 ease-in-out translate-y-[-50px] opacity-0">
     <div class="wrapper font-Poppins">
-      <nav class="flex justify-between items-center text-primary w-full px-20 py-6 ">
+      <nav class="flex justify-between items-center text-primary w-full px-20 py-6">
         <h1 class="font-knewave text-2xl">Ananda</h1>
         <div class="flex flex-row gap-8">
           <h2 @click="scrollToSection('aboutSection')" class="cursor-pointer">About</h2>
@@ -111,7 +210,12 @@ onMounted(() => {
         Third-year University student at Mahidol University Full-stack Developer <br />
         No one hires me. So, I have to make this website.
       </p>
-      <h1 class="font-knewave text-black text-[160px] transition-all ease-in-out translate-y-[-70px] opacity-0 delay-700" id="myName">ANANDA</h1>
+      <h1
+        class="font-knewave text-black text-[160px] transition-all ease-in-out translate-y-[-70px] opacity-0 delay-700"
+        id="myName"
+      >
+        ANANDA
+      </h1>
       <div class="scroll-down flex flex-col justify-center items-center mt-20">
         <p class="text-secondary text-xl font-semibold animate-bounce">Scroll Down</p>
         <HiChevronDoubleDown class="animate-bounce text-black" size="36" />
@@ -148,7 +252,9 @@ onMounted(() => {
         />
       </div>
     </section>
-    <section class="flex flex-col justify-start lg:items-start w-full min-h-[500px] p-10 bg-pink-bg mb-40 sm:items-center">
+    <section
+      class="flex flex-col justify-start lg:items-start w-full min-h-[500px] p-10 bg-pink-bg mb-40 sm:items-center"
+    >
       <h1 class="text-4xl font-bold mb-10 ml-23">Skills</h1>
       <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-7 gap-20">
         <div
@@ -179,6 +285,22 @@ onMounted(() => {
       <h1 class="text-4xl text-left ml-23">Projects</h1>
       <div v-for="(project, index) in projects" :key="index" class="w-full">
         <ProjectComponent :ProjectData="project" />
+      </div>
+    </section>
+    <section class="flex flex-col justify-start items-start w-full min-h-fit p-10 gap-10">
+      <h1 class="text-4xl text-left ml-23">The Odin Project</h1>
+      <div class="flex flex-row flex-wrap justify-center items-center gap-10">
+        <div v-for="(project, index) in projectImagesODIN" :key="index" class="w-fit">
+          <ProjectComponentNoD :ProjectDataNoD="project" />
+        </div>
+      </div>
+    </section>
+    <section class="flex flex-col justify-start items-start w-full min-h-fit p-10 gap-10">
+      <h1 class="text-4xl text-left ml-23">Frontend Mentor</h1>
+      <div class="flex flex-row flex-wrap justify-center items-center gap-10">
+        <div v-for="(project, index) in projectImagesFM" :key="index" class="w-fit">
+          <ProjectComponentNoD :ProjectDataNoD="project" />
+        </div>
       </div>
     </section>
 
